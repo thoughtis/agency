21ff05dd1518f5c1b891010eb1955ec1492dbddb
Jared Salzano <jaredsalzano@gmail.com>
2021-05-23 18:15:02 -0400

    Building ...

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+
 // Definitions
 const toggleShowAtHoverPosition = (e, showOrHide) => {
   const elt = e.target.querySelector('.show-at-hover-position__elt-to-show');
@@ -21,3 +22,36 @@ document.querySelectorAll('.show-at-hover-position__target').forEach((target) =>
   target.addEventListener('mouseover', e => toggleShowAtHoverPosition(e, 'show'))
   target.addEventListener('mouseout', e => toggleShowAtHoverPosition(e, 'hide'))
 })
+
+// Strategy shapes - animate on scroll
+ScrollTrigger.create({
+  trigger: ".strategy-shapes",
+  start: "top bottom",
+  onEnter: () => {
+    // Fade in the strategy shapes, one by one
+    let delay = 0
+
+    document.querySelectorAll('.strategy-shapes > div').forEach(shapeElt => {
+      if (shapeElt && shapeElt.classList) {
+        setTimeout(() => {
+          shapeElt.classList.remove('opacity-0')
+          shapeElt.classList.add('opacity-100')
+        }, delay) 
+        
+        delay = delay + 400
+      }
+    })
+
+    // Now Fade in the dots
+    document.querySelectorAll('.strategy-shapes__dotted-line').forEach(dotsElt => {
+      if (dotsElt && dotsElt.classList) {
+        setTimeout(() => {
+          dotsElt.classList.remove('opacity-0')
+          dotsElt.classList.add('opacity-100')
+        }, delay) 
+        
+        delay = delay + 100
+      }
+    })
+  }
+});

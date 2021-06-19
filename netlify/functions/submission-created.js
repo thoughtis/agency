f8fafc606ff649e3b33bbbf0b8cb0833ab4c67b6
Jared Salzano <jaredsalzano@gmail.com>
2021-06-19 09:56:24 -0400

    Update function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,6 +1,5 @@
 exports.handler = async function(event, context) {
-  console.log("Hello function");
-  const formValues = event.body.payload
+  const formValues = JSON.parse(event.body).payload;
   console.log(formValues);
   return { statusCode: 200 };
 }
